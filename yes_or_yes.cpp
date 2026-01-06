009a5ca3c402eb323f5330265069e7181118c5bf
Parul2507 <parulbudhori2003@gmail.com>
2026-01-06 22:36:24 +0530

    Problem on strings comparession

---
+++ yes_or_yes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        for (int i = 0; i < s.length(); i++)
+        {
+            s[i] = toupper (s[i]);
+        }
+        if (s == "YES")
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+}
