8516442cc06f70ff72f6f37f905d105f76f6b1fb
Parul2507 <parulbudhori2003@gmail.com>
2026-01-09 23:55:52 +0530

    Problem on 'set'

---
+++ horseshoe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main()
+{
+    set<int> s;
+    int k, n = 4;
+    while (n--)
+    {
+        cin>>k;
+        s.insert(k);
+    }
+    cout<<4-s.size()<<endl;
+}
