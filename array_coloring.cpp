e366d78665e629166f8d169a578f3e446eea579d
Parul2507 <parulbudhori2003@gmail.com>
2026-01-23 12:54:28 +0530

    Array and math problem

---
+++ array_coloring.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        int count_odd = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 == 1)
+            {
+                count_odd++;
+            }
+        }
+        if (count_odd % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" <<endl;
+        }
+    }
+}
