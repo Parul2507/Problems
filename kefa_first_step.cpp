a9717d9e3b73e0d5dffd35fa578da89ecdcf11e5
Parul2507 <parulbudhori2003@gmail.com>
2026-02-24 22:56:55 +0530

    Codeforce problem kadane algo

---
+++ kefa_first_step.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    int count = 1, maxcount = 1;
+    for(int i=0; i<n-1; i++)
+    {
+        if(arr[i] <= arr[i+1])
+        {
+            count++;
+        }
+        else
+        {
+            count = 1;
+        }
+        if(count > maxcount)
+        {
+            maxcount = count;
+        }
+    }
+    cout << maxcount << endl;
+}
