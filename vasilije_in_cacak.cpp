00b58ab0a06dc1bb4c1503cb1514946a4f489281
Parul2507 <parulbudhori2003@gmail.com>
2026-04-01 22:11:26 +0530

    Minor changes

--- vasilije_in_cacak.cpp
+++ vasilije_in_cacak.cpp
@@ -8,8 +8,8 @@ int main()
     {
         long long n, k, x;
         cin >> n >> k >> x;
-        long low = (k*(k+1))/2; 
-        long high = (n*(n+1))/2 - ((n-k)*(n-k+1))/2;
+        long long low = (k*(k+1))/2; 
+        long long high = (n*(n+1))/2 - ((n-k)*(n-k+1))/2;
         if(x >= low && x <= high)
         {
             cout << "YES" << endl;
