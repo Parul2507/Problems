07a7c8e19cc2cadb139b696076fa7509310d3089
Parul2507 <parulbudhori2003@gmail.com>
2026-04-12 18:48:12 +0530

    Permutation problem from codeforce

---
+++ jagged_swapped.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+        }
+        if(arr[0] == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
