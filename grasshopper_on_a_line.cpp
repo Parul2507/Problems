4c35e4cd1c798f2bb3aa0bc0b81968edae81a70c
Parul2507 <parulbudhori2003@gmail.com>
2026-04-23 23:30:48 +0530

    Math

---
+++ grasshopper_on_a_line.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        long long x, k;
+        cin >> x >> k;
+        if(x%k == 0)
+        {
+            cout << 2 << endl;
+            cout << 1 << " " << x-1 << endl;
+        }
+        else
+        {
+            cout << 1 << endl;
+            cout << x << endl;
+        }
+    }
+}
